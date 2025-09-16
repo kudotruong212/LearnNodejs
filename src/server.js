a9ef7b3b279c90b26b9ecc741fac013a5e58ea4b
đặng Danh <danh2492004@gmail.com>
2025-09-16 22:35:26 +0700

    learn env

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 // import express from 'express'
 const app = express();
-const port = 8080;
+require('dotenv').config()
+const port = process.env.PORT || 3000;
+const path = require('path')
+const hostname = process.env.HOST_NAME
+
+// config template engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 
+console.log(process.env)
 // khai bao route
 app.get('/abc', (req, res) => {
   res.send('Hello World!')
@@ -14,7 +22,7 @@ app.get('/xinchao', (req, res) =>{
 })
 
 app.get('/text', (req, res) =>{
-  res.send("<h1>Day la the h1</h1><h2>Day la the h2</h2>")
+  res.render('sample.ejs')
 })
 
 
