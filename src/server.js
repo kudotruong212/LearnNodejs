37a90c9a2d8c1a2ffd5d97a61b2dc3c7a4e85618
đặng Danh <danh2492004@gmail.com>
2025-09-17 00:40:14 +0700

    public_file

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ const hostname = process.env.HOST_NAME
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+//config static files
+app.use(express.static(path.join(__dirname, 'public')))
 
 // khai bao route
 app.get('/abc', (req, res) => {
